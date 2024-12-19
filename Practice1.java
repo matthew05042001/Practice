84af4583c441c8727841a6cb00f04943c2a9e91f
matthew05042001 <mcrispino_240000000173@uic.edu.ph>
2024-12-19 16:39:57 +0800

    Add files via upload

---
+++ Practice1.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practice1;
+import java.util.Scanner;
+
+/**
+ *
+ * @author matth
+ */
+public class Practice1 {
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+             
+       
+        
+        int cases;
+        System.out.println("Enter number of test Cases:");
+        cases = inp.nextInt();
+        double [] number = new double [cases]; 
+        
+        double sum = 0; 
+        
+        for (int i = 0; i < cases; i++) { 
+            System.out.println(i + 1 + "#"  );
+            number[i] = inp.nextDouble();
+            sum += number[i];
+           
+          
+            
+        }
+        
+        double average = (double) sum / cases;
+        
+        System.out.printf("Average  = %.2f\n", average);
+        System.out.println("is the minimum " + number[0]);
+        System.out.println("is the maximum " + number[number.length - 2 ]);
+        
+        
+
+    }
+}
